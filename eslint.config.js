c327cc7bb6f1672649a955162f0aacfbeae7a73d
GiaBao <duonggiabao254@gmail.com>
2025-07-26 23:22:08 +0700

    create base

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(["dist", "node_modules", "vite.config.ts"]),
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+        ],
+        plugins: {
+            react,
+            prettier: eslintPluginPrettier,
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "react/jsx-no-target-blank": "warn",
+            "react-refresh/only-export-components": "off",
+            "react/prop-types": "off",
+            "prettier/prettier": [
+                "warn",
+                {
+                    arrowParens: "always",
+                    semi: true,
+                    trailingComma: "es5",
+                    tabWidth: 4,
+                    endOfLine: "auto",
+                    useTabs: false,
+                    singleQuote: false,
+                    printWidth: 80,
+                    jsxSingleQuote: true,
+                },
+            ],
+        },
     },
-  },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    css: {
+        devSourcemap: true,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    // server: {
+    //     allowedHosts: [
+    //         "f4a4-2001-ee0-519a-62e0-d9db-7627-d09-c524.ngrok-free.app",
+    //     ],
+    // },
+});
