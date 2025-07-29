b7270dd0fa1199fa69b4f3d0c6eb76698fca2c8a
GiaBao <duonggiabao254@gmail.com>
2025-07-29 23:39:49 +0700

    Add react-toastify for notification handling and implement error toast in HTTP response interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ToastContainer } from "react-toastify";
 import useRouteElements from "./useRouteElements";
 
 export default function App() {
     const routeElements = useRouteElements();
-    return <div>{routeElements}</div>;
+    return (
+        <div>
+            {routeElements}
+            <ToastContainer />
+        </div>
+    );
 }
