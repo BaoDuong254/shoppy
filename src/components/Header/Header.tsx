1ebfae7380750d97a78436cf2e4c9bf28c91eaa1
GiaBao <duonggiabao254@gmail.com>
2025-08-03 10:59:22 +0700

    fix: improve logout handling and update purchases query logic in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,6 +14,7 @@ import { purchaseStatus } from "@constants/purchase";
 import purchaseApi from "@apis/purchase.api";
 import { formatCurrency } from "@utils/utils";
 import noProduct from "@assets/images/no-product.png";
+import { queryClient } from "@/main";
 
 type FormData = Pick<Schema, "name">;
 const nameSchema = schema.pick(["name"]);
@@ -37,12 +38,21 @@ export default function Header() {
     onSuccess: () => {
       setIsAuthenticated(false);
       setProfile(null);
+      queryClient.removeQueries({
+        queryKey: [
+          "purchases",
+          {
+            status: purchaseStatus.inCart,
+          },
+        ],
+      });
     },
   });
 
   const { data: purchasesInCartData } = useQuery({
     queryKey: ["purchases", { status: purchaseStatus.inCart }],
     queryFn: () => purchaseApi.getPurchases({ status: purchaseStatus.inCart }),
+    enabled: isAuthenticated,
   });
 
   const purchasesInCart = purchasesInCartData?.data.data;
@@ -238,7 +248,7 @@ export default function Header() {
                       </div>
                     </div>
                   ) : (
-                    <div className='flex h-[300px] w-[300px] items-center justify-center p-2'>
+                    <div className='flex h-[300px] w-[300px] flex-col items-center justify-center p-2'>
                       <img src={noProduct} alt='no product' className='h-24 w-24' />
                       <div className='mt-3 capitalize'>Chưa có sản phẩm</div>
                     </div>
@@ -261,9 +271,11 @@ export default function Header() {
                     d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
                   />
                 </svg>
-                <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
-                  {purchasesInCart?.length}
-                </span>
+                {purchasesInCart && (
+                  <span className='absolute left-[17px] top-[-5px] rounded-full bg-white px-[9px] py-[1px] text-xs text-orange'>
+                    {purchasesInCart?.length}
+                  </span>
+                )}
               </Link>
             </Popover>
           </div>
