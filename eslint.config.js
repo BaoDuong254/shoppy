68aa46c79a42dd44fc8648b7e19045d3b65672d1
GiaBao <duonggiabao254@gmail.com>
2025-08-06 15:52:52 +0700

    Add storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -8,45 +11,48 @@ import eslintPluginPrettier from "eslint-plugin-prettier";
 import react from "eslint-plugin-react";
 import tsdoc from "eslint-plugin-tsdoc";
 
-export default tseslint.config([
-  globalIgnores(["dist", "node_modules", "vite.config.ts", "coverage", "stats.html", "__snapshots__"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    plugins: {
-      react,
-      prettier: eslintPluginPrettier,
-      tsdoc,
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-no-target-blank": "warn",
-      "react-refresh/only-export-components": "off",
-      "react/prop-types": "off",
-      "tsdoc/syntax": "warn",
-      "prettier/prettier": [
-        "warn",
-        {
-          arrowParens: "always",
-          semi: true,
-          trailingComma: "es5",
-          tabWidth: 2,
-          endOfLine: "auto",
-          useTabs: false,
-          singleQuote: false,
-          printWidth: 120,
-          jsxSingleQuote: true,
-        },
+export default tseslint.config(
+  [
+    globalIgnores(["dist", "node_modules", "vite.config.ts", "coverage", "stats.html", "__snapshots__"]),
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs["recommended-latest"],
+        reactRefresh.configs.vite,
       ],
+      plugins: {
+        react,
+        prettier: eslintPluginPrettier,
+        tsdoc,
+      },
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-no-target-blank": "warn",
+        "react-refresh/only-export-components": "off",
+        "react/prop-types": "off",
+        "tsdoc/syntax": "warn",
+        "prettier/prettier": [
+          "warn",
+          {
+            arrowParens: "always",
+            semi: true,
+            trailingComma: "es5",
+            tabWidth: 2,
+            endOfLine: "auto",
+            useTabs: false,
+            singleQuote: false,
+            printWidth: 120,
+            jsxSingleQuote: true,
+          },
+        ],
+      },
     },
-  },
-]);
+  ],
+  storybook.configs["flat/recommended"]
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,6 +6,11 @@ import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 // https://vite.dev/config/
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), visualizer()],
   css: {
@@ -13,6 +19,32 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: path.resolve(__dirname, "./vitest.setup.js"),
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
   },
   // server: {
   //     allowedHosts: [
